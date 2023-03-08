b240db705005e113fa669eace518a2f89204a0f6
Kaanayden <kaanayden15@gmail.com>
2023-03-08 22:30:23 +0300

    Add react native firebase and cloud firestore

--- app/src/screens/Login.jsx
+++ app/src/screens/Login.jsx
@@ -17,9 +17,18 @@ import SignUp from './SignUp';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import firestore from '@react-native-firebase/firestore';
+
 export default function Login(props) {
 
     const { text, navigation } = props;
+
+    async function saveToFirestore() {
+        await firestore().collection('Tests').add({
+            name:"Test1",
+            age: 25,
+        })
+    }
 
     return (
 
@@ -27,6 +36,7 @@ export default function Login(props) {
             <Text>Sign In</Text>
             <Button onPress={ () => navigation.navigate( "SignUp" ) }>Navigate</Button>
             <Icon name="rocket" size={30} color="#900" />
+            <Button onPress={saveToFirestore}>Save to Firestore</Button>
         </View>
     );
 
