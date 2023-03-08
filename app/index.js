0147acd67386053880b8e4ff38896ded821a3826
Kaanayden <kaanayden15@gmail.com>
2023-03-08 21:18:19 +0300

    Add top tab and drawer

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@
  * @format
  */
 
+import 'react-native-gesture-handler';
+//it should be in top always
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
