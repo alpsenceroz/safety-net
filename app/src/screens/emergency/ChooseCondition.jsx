e580db6eb4b673ee67fd7393ed01818e2c6f6101
alpsencer <28690608+alpsencer@users.noreply.github.com>
2023-03-30 12:19:19 +0300

    fix hidden content by keyboard issue

--- app/src/screens/emergency/ChooseCondition.jsx
+++ app/src/screens/emergency/ChooseCondition.jsx
@@ -7,7 +7,9 @@ import {
     Text,
     useColorScheme,
     View,
-    Image
+    Image,
+    KeyboardAvoidingView,
+    Keyboard
 } from 'react-native';
 
 import {
@@ -19,8 +21,10 @@ import {
 import globalStyles from '../../utils/Styles';
 
 
-
 const ChooseCondition = ({route, navigation}) => {
+    useEffect(() => {
+        navigation.setOptions({ title: 'Choose Condition' });
+      }, []);
     // const [emergency, setEmergency] = useState(route.params.emergency);
     const [evacuation, setEvacuation] = useState(emergency.needEvacuation)
     const [injured, setInjured] = useState(emergency.isInjured)
@@ -33,22 +37,53 @@ const ChooseCondition = ({route, navigation}) => {
     // const handleCheckBox = (key) =>{
     //     setEmergency((prevEmergency) => ({...prevEmergency, [key]: !prevEmergency[key]}))
     // }
+    const [keyboardHeight, setKeyboardHeight] = useState(0);
 
+    useEffect(() => {
+      const keyboardDidShowListener = Keyboard.addListener(
+        'keyboardDidShow',
+        (event) => {
+          setKeyboardHeight(event.endCoordinates.height);
+        }
+      );
+  
+      const keyboardDidHideListener = Keyboard.addListener(
+        'keyboardDidHide',
+        () => {
+          setKeyboardHeight(0);
+        }
+      );
+  
+      return () => {
+        keyboardDidShowListener.remove();
+        keyboardDidHideListener.remove();
+      };
+    }, []);
  
     emergency = route.params.emergency
 
         return(
-
+<ScrollView
+      contentContainerStyle={{
+        flexGrow: 1,
+        paddingBottom: keyboardHeight,
+        backgroundColor: '#FAE3D9',
+      }}
+    >
             <View style={{flex: 1}}backgroundColor = '#FAE3D9'>
+                 
                 <Image
-        style={{width: 350, height:300, alignSelf:'center', marginTop:80, marginBottom:80}}
+        style={{width: 300, height:300, alignSelf:'center', marginTop:40, marginBottom:40}}
         source={require('../../assets/condition.png')}
       />
-                <View style={{backgroundColor:'#ffffff'}}>
+               
+                    
+                
+                <View style={{backgroundColor:'#ffffff', marginHorizontal: 20}}>
                 <Checkbox.Item
                 label="Need Evacuation"
                 color='#e90064'
-                style={{marginTop: 0, backgroundColor:'#ffffff'}}
+                style={{marginTop: 0, backgroundColor:'#ffffff', marginVertical:10}}
                 status={evacuation ? 'checked' : 'unchecked'}
                 onPress={() => { 
                     // handleCheckBox("needEvacuation")
@@ -59,6 +94,7 @@ const ChooseCondition = ({route, navigation}) => {
                 <Checkbox.Item
                 label="Injured"
                 color='#e90064'
+                style={{marginTop: 0, backgroundColor:'#ffffff', marginVertical:10}}
                 status={injured ? 'checked' : 'unchecked'}
                 onPress={() => { 
                     // handleCheckBox("isInjured")
@@ -74,13 +110,15 @@ const ChooseCondition = ({route, navigation}) => {
                     onChangeText={(text)=>emergency.notes = text}
                     />
                     </View>
-                <Button buttonColor= {globalStyles.button1.buttonColor} textColor={globalStyles.button1.textColor} style={ {...globalStyles.button1.style, marginTop: 20, alignSelf: 'center', width: 100, justifyContent:'center'} }onPress= {() => {
+                <Button buttonColor= {globalStyles.button1.buttonColor} textColor={globalStyles.button1.textColor} style={ {...globalStyles.smallAddButton.style, marginTop: 20, alignSelf: 'center', width: 100, justifyContent:'center'} }onPress= {() => {
                     navigation.replace("EmergencyReported", {emergency: emergency})
                    //navigation.reset()
 
                 
             }}>Continue</Button>
             </View>
+            </ScrollView>
+
         )
 
 }
