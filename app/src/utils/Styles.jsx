9728333493cb919b082d3d365881e899be1eb65e
alpsencer <28690608+alpsencer@users.noreply.github.com>
2023-03-30 17:46:30 +0300

    fix global style

--- app/src/utils/Styles.jsx
+++ app/src/utils/Styles.jsx
@@ -3,186 +3,24 @@ import { StyleSheet } from "react-native";
 
 const globalStyles={
     map:[
-        {
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#e9e9e9"
-                },
-                {
-                    "lightness": 17
-                }
-            ]
-        },
-        {
-            "featureType": "landscape",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#f5f5f5"
-                },
-                {
-                    "lightness": 20
-                }
-            ]
-        },
-        {
-            "featureType": "road.highway",
-            "elementType": "geometry.fill",
-            "stylers": [
-                {
-                    "color": "#ffffff"
-                },
-                {
-                    "lightness": 17
-                }
-            ]
-        },
-        {
-            "featureType": "road.highway",
-            "elementType": "geometry.stroke",
-            "stylers": [
-                {
-                    "color": "#ffffff"
-                },
-                {
-                    "lightness": 29
-                },
-                {
-                    "weight": 0.2
-                }
-            ]
-        },
-        {
-            "featureType": "road.arterial",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#ffffff"
-                },
-                {
-                    "lightness": 18
-                }
-            ]
-        },
-        {
-            "featureType": "road.local",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#ffffff"
-                },
-                {
-                    "lightness": 16
-                }
-            ]
-        },
-        {
-            "featureType": "poi",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#f5f5f5"
-                },
-                {
-                    "lightness": 21
-                }
-            ]
-        },
-        {
-            "featureType": "poi.park",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#dedede"
-                },
-                {
-                    "lightness": 21
-                }
-            ]
-        },
-        {
-            "elementType": "labels.text.stroke",
-            "stylers": [
-                {
-                    "visibility": "on"
-                },
-                {
-                    "color": "#ffffff"
-                },
-                {
-                    "lightness": 16
-                }
-            ]
-        },
-        {
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "saturation": 36
-                },
-                {
-                    "color": "#fc2e63"
-                },
-                {
-                    "lightness": 40
-                }
-            ]
-        },
-        // {
-        //     "elementType": "labels.icon",
-        //     "stylers": [
-        //         {
-        //             "visibility": "off"
-        //         }
-        //     ]
-        // },
-        {
-            "featureType": "transit",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#f2f2f2"
-                },
-                {
-                    "lightness": 19
-                }
-            ]
-        },
-        {
-            "featureType": "administrative",
-            "elementType": "geometry.fill",
-            "stylers": [
-                {
-                    "color": "#fefefe"
-                },
-                {
-                    "lightness": 20
-                }
-            ]
-        },
-        {
-            "featureType": "administrative",
-            "elementType": "geometry.stroke",
-            "stylers": [
-                {
-                    "color": "#fefefe"
-                },
-                {
-                    "lightness": 17
-                },
-                {
-                    "weight": 1.2
-                }
-            ]
-        }
+        
     ],
     button1: {
         buttonColor:'#e90064',
         textColor:'#ffffff',
         style:{ 
     }},
+    mainView: {
+        flex: 1, 
+        backgroundColor:'#FFFFFF',
+    },
+    editView: {
+        flex: 1, 
+        backgroundColor:'#FFFFFF',
+        marginHorizontal: 20,
+        justifyContent: 'flex-end',
+        marginBottom: '20%'
+    },
     screenAddButton: {
         textColor:'#ffffff',
         style:{
@@ -191,7 +29,42 @@ const globalStyles={
             backgroundColor: 'black'
     }},
     smallAddButton: {
-        textColor:'#ffffff',
+        marginVertical: 5,
+        borderRadius: 10,
+        alignSelf: 'center', 
+        width: 200, 
+        justifyContent:'center',
+    },
+    smallAddButtonHorizontal: {
+        marginVertical: 5,
+        borderRadius: 10,
+        alignSelf: 'center', 
+        width: '30%', 
+        justifyContent:'center',
+    },
+    addButtonHorizontalHalfScreen: {
+        marginVertical: 5,
+        marginRight: 2,
+        borderRadius: 10,
+        alignSelf: 'center', 
+        width: '40%', 
+        marginTop: '5%'
+    },
+    addButtonHorizontalQuarterScreen: {
+        marginVertical: 5,
+        marginHorizontal: 2,
+        borderRadius: 10,
+        alignSelf: 'center', 
+        width: '30%', 
+        marginTop: '5%'
+    },
+    smallAddButtonBlack: {
+        marginVertical: 10,
+        borderRadius: 10,
+        backgroundColor: 'black',
+        alignSelf: 'center', 
+        width: 200, 
+        justifyContent:'center',
         style:{
             marginVertical: 30,
             borderRadius: 10,
