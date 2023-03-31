ffcfb953138b21344ed41d299928f01b2116a51f
Alpsencer <39705711+Alpsencer68@users.noreply.github.com>
2023-03-31 06:46:26 +0300

    Add profile avatar and logout icon

--- app/src/screens/HomeScreen/Home.jsx
+++ app/src/screens/HomeScreen/Home.jsx
@@ -7,14 +7,16 @@ import {
     Text,
     useColorScheme,
     View,
+    Pressable
 } from 'react-native';
 
 import {
-    Button
+    Button,
+    Avatar
 } from 'react-native-paper';
 
 
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
@@ -26,6 +28,21 @@ export default function Home({navigation}) {
 
 
     const user = auth().currentUser;
+    const [userName, setUserName] = useState(false)
+
+
+    useEffect(()=>{
+    const userSub = firestore()
+      .collection('users').doc(user.uid)
+      .onSnapshot((querySnapshot) => {
+        console.log(querySnapshot.get('name'))
+        setUserName(querySnapshot.get('name'))
+      })
+      return () => {
+        userSub()
+      }
+    }
+    ,[])
 
 
     async function handleLogOut() {
@@ -73,10 +90,33 @@ export default function Home({navigation}) {
       
     return (
 
-        <View>
-            <Button onPress={handleLogOut}>Log out</Button>
+        <View style={{ flex: 1, backgroundColor:'#FFFFFF' }}>
+
+            <View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: 20}}>
+            <Pressable style={{flexDirection: 'row'}}
+            onPress={() =>{
+                navigateProfile()
+                }
+            }>
+            <Avatar.Image 
+            style={{alignSelf: 'flex-start', marginStart: 20}} 
+            size={44} source={require('../../assets/user.png')} 
+            />
+            {userName ? <Text style={{marginTop: 9, marginLeft: 10, color: "#000000", fontSize: 16}}>{userName}</Text>: <></>}
+            </Pressable>
+
+            <Pressable style={{marginTop: 5, marginRight: 10}}
+            onPress={() =>{
+                handleLogOut()
+                }
+            }>
+            <Icon name="door-open" size={25} color='#000000' ></Icon>
+            </Pressable>
+
+            </View>
+            {/* <Button onPress={handleLogOut}>Log out</Button> */}
             <Button onPress={handleEmergencyButton}>Help</Button>
-            <Button onPress={ navigateProfile }>Edit Profile</Button>
+            {/* <Button onPress={ navigateProfile }>Edit Profile</Button> */}
 
         </View>
     );
